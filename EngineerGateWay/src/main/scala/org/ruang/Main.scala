fb5fcb79bf844e6efe9baed75dd65446b2c53bec
ruang <longjinyii@outlook.com>
2023-11-28 16:48:54 +0800

    完善了请求转发

--- EngineerGateWay/src/main/scala/org/ruang/Main.scala
+++ EngineerGateWay/src/main/scala/org/ruang/Main.scala
@@ -1,12 +1,22 @@
 package org.ruang
 
-import com.twitter.finagle.Http
+import com.twitter.finagle.http.filter.Cors
 import com.twitter.util.Await
+import org.moon.http.CorsSetting
 import org.ruang.router.RequestRouter
 
 object Main {
+  private def setting: Cors.Policy = Cors.Policy(
+    allowsOrigin = _ => Some("*"),
+    allowsMethods = _ => Some(Seq("GET", "POST", "PUT", "DELETE")),
+    allowsHeaders = _ => Some(Seq("Content-Type")))
+
   def main(args: Array[String]): Unit = {
-    val requestRouter = Http.serve(":8080", new RequestRouter)
-    Await.ready(requestRouter)
+    // 设置跨域
+    val servers = new CorsSetting()
+      .add(new RequestRouter, "8080")
+      .apply()
+    // 启动服务
+    servers.foreach(x => Await.result(x))
   }
 }
--- EngineerGateWay/src/main/scala/org/ruang/router/RequestRouter.scala
+++ EngineerGateWay/src/main/scala/org/ruang/router/RequestRouter.scala
@@ -4,8 +4,11 @@ import com.twitter.finagle.http.path._
 import com.twitter.finagle.http.{Request, Response, Status}
 import com.twitter.util.Future
 import org.moon.common.Json
+import org.moon.common.config.Configure
 import org.moon.http.{ApiGateway, ForwardRequest}
+import org.ruang.discover.ZkServerDiscover
 
+import scala.collection.mutable.ListBuffer
 
 /**
  * API网关
@@ -13,16 +16,33 @@ import org.moon.http.{ApiGateway, ForwardRequest}
 class RequestRouter extends ApiGateway {
   override def apply(request: Request): Future[Response] = {
     val response = Response()
+    response.setContentTypeJson()
 
-    // 路由选择可以优化为路径参数
+    // 获取服务列表
+    val config = Configure()
+    val list = config.getList("servers")
+    // ListBuffer(("userAuth", "localhost:50010"), ("tempNote", "localhost:50020"))
+    val servers = ListBuffer[(String, String)]()
 
+    // 服务发现
+    val zkCenter = new ZkServerDiscover("192.168.10.137", "2181")
+    list.forEach(x => {
+      val name = x.unwrapped().toString
+      // 将服务状态和路由存储到servers中
+      servers.append(zkCenter.discover(name))
+    })
+
+    // 请求转发
     Path(request.path) match {
-      // 将请求转发到后端login RESTapi
-      case Root / "login" => forwardRequest("localhost", "50010", request, response)
-      // 转发请求到后端register RESTapi
-      case Root / "register" => forwardRequest("localhost", "50010", request, response)
-      // 转发请求到后端tempNote RESTapi
-      case Root / "note" / _ => forwardRequest("localhost", "50020", request, response)
+      // 将请求转发到后端 login RESTapi
+      case Root / "login" =>
+        forwardRequest(servers.find(_._1 == "userAuth"), request, response)
+      // 转发请求到后端 register RESTapi
+      case Root / "register" =>
+        forwardRequest(servers.find(_._1 == "userAuth"), request, response)
+      // 转发请求到后端 tempNote RESTapi
+      case Root / "note" / _ =>
+        forwardRequest(servers.find(_._1 == "tempNote"), request, response)
       // 异常访问
       case _ =>
         response.setContentString(Json.of(Map("message" -> "not found")))
@@ -31,14 +51,26 @@ class RequestRouter extends ApiGateway {
     Future.value(response)
   }
 
-  private def forwardRequest(host: String, port: String,
-                             request: Request, response: Response): Unit = {
-    new ForwardRequest()
-      .send(host, port, request)
-      .process({ x =>
-        response.setContentString(x.getContentString())
-      })
-      .close
-  }
+  /**
+   * 转发请求
+   *
+   * @param server   服务元组(服务名称, 服务地址)
+   * @param request  请求
+   * @param response 响应
+   */
 
+  private def forwardRequest(server: Option[(String, String)],
+                             request: Request, response: Response): Unit = {
+    server.foreach { case (_, hostPort) =>
+      // 获取主机名和端口
+      val Array(host, port) = hostPort.split(":")
+      // 转发到对应服务
+      new ForwardRequest()
+        .send(host, port, request)
+        .process { x =>
+          response.setContentString(x.getContentString())
+        }
+        .close
+    }
+  }
 }
