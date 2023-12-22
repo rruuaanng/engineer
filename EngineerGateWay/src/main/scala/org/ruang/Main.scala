7521a8497c74d5dfba52612cf579a0176a1fdca2
ruang <longjinyii@outlook.com>
2023-12-22 22:28:25 +0800

    没啥

--- EngineerGateWay/src/main/scala/org/ruang/Main.scala
+++ EngineerGateWay/src/main/scala/org/ruang/Main.scala
@@ -1,22 +1,26 @@
 package org.ruang
 
-import com.twitter.finagle.http.filter.Cors
-import com.twitter.util.Await
-import org.moon.http.CorsSetting
-import org.ruang.router.RequestRouter
+import com.twitter.finagle.Http
+import com.twitter.finagle.http.{Request, Response}
+import com.twitter.util.Future
+import org.moon.http.RestAPI
 
 object Main {
-  private def setting: Cors.Policy = Cors.Policy(
-    allowsOrigin = _ => Some("*"),
-    allowsMethods = _ => Some(Seq("GET", "POST", "PUT", "DELETE")),
-    allowsHeaders = _ => Some(Seq("Content-Type")))
-
   def main(args: Array[String]): Unit = {
+    val api = new RestAPI {
+      override def apply(request: Request): Future[Response] = {
+        val response = Response()
+        response.setContentString("hello world")
+        Future.value(response)
+      }
+    }
+    Http.serve(":8080", api)
+    while (true) {}
     // 设置跨域
-    val servers = new CorsSetting()
-      .add(new RequestRouter, "8080")
-      .apply()
-    // 启动服务
-    servers.foreach(x => Await.result(x))
+    //    val servers = new CorsSetting()
+    //      .add(new RequestRouter, "8080")
+    //      .apply()
+    //    // 启动服务
+    //    servers.foreach(x => Await.result(x))
   }
 }
